3e2faedb9a3f34370c38f5fa36b8afa45ef794fd
Aditya Verma <adityaverma6300@gmail.com>
2026-05-19 15:41:58 +0530

    Add files via upload

---
+++ news-site/admin/footer.php
@@ -0,0 +1,13 @@
+<!-- Footer -->
+<div id ="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <span>© Copyright 2026 News | Powered by <a href="https://github.com/AD1TYAverma" target="_blank">Aditya Verma</a></span>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- /Footer -->
+</body>
+</html>
